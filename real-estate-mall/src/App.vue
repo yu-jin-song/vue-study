13d0670f132c5023bdd63ce7f0bd22cb4b81d370
yu-jin-song <yujinsong.tech@gmail.com>
2024-01-25 19:07:38 +0900

    Apply for-loop

--- real-estate-mall/src/App.vue
+++ real-estate-mall/src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4 class="red" :style="fontColor">{{ products[0] }}</h4>
-    <p>{{ prices[0] }} 만원</p>
+
+  <!-- 상단 내비게이션 메뉴 -->
+  <div class="menu">
+    <!-- 1. <태그 v-for="작명 in 반복횟수" :key="작명"> -->
+    <!-- <a v-for="menu in 3" :key="menu">Home</a> -->
+
+    <!-- 2. <태그 v-for="작명 in 배열" :key="작명"> {{ 작명 }} -->
+    <a v-for="menu in menus" :key="menu"> {{ menu }} </a>
+
+    <!-- 3. <태그 v-for="(작명(n),순서(i)) in 배열" :key="작명"> {{ 작명/순서 }} -->
+    <!-- <a v-for="(menu,i) in menus" :key="menu"> {{ menu }},{{ i }} </a> -->
   </div>
-  <div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  <div v-for="(item, i) in 3" :key="item">
+    <h4 class="red" :style="fontColor">{{ products[i] }}</h4>
+    <p>{{ prices[i] }} 만원</p>
+  </div>
+  <!-- 다른 방법
+        <div v-for="(item, i) in products" :key="item">
+          <h4 class="red" :style="fontColor">{{ item }}</h4>
+          <p>{{ prices[i] }} 만원</p>
+        </div>
+  -->
+  <!-- <div>
     <h4>{{ products[1] }}</h4>
     <p>{{ prices[1] }} 만원</p>
   </div>
   <div>
     <h4 class="red" :style="fontColor">{{ products[2] }}</h4>
     <p>{{ prices[2] }} 만원</p>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -25,6 +45,7 @@ export default {
       prices : [50, 60, 70],  // 자료이름 : 내용
       fontColor : "color : blue",
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menus : ['Home', 'Shop', 'About']
     }
   },
   components: {
@@ -39,6 +60,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a { /* a 태그 속성 적용 */
+  color: white;
+  padding: 10px;
 }
 </style>
