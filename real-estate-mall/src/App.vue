683e74c131ddf4f074f381b4a548a6a5432266e3
yu-jin-song <yujinsong.tech@gmail.com>
2024-01-25 18:04:42 +0900

    Study data binding

--- real-estate-mall/src/App.vue
+++ real-estate-mall/src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 class="red" :style="fontColor">XX 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// document.getElementById().innerHTML = ?; js 데이터 바인딩 방법
 
 export default {
   name: 'App',
+  data() {  // 데이터 보관, 자료형은 object
+    return {
+      price1 : 60,  // 자료이름 : 내용
+      price2 : 70,
+      fontColor : "color : blue",
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
