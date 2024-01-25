46104ee9610000cb6b44db73937e322eb4a83cd0
yu-jin-song <yujinsong.tech@gmail.com>
2024-01-25 19:59:39 +0900

    feat: Add report button

--- real-estate-mall/src/App.vue
+++ real-estate-mall/src/App.vue
@@ -2,50 +2,41 @@
 
   <!-- 상단 내비게이션 메뉴 -->
   <div class="menu">
-    <!-- 1. <태그 v-for="작명 in 반복횟수" :key="작명"> -->
-    <!-- <a v-for="menu in 3" :key="menu">Home</a> -->
-
-    <!-- 2. <태그 v-for="작명 in 배열" :key="작명"> {{ 작명 }} -->
     <a v-for="menu in menus" :key="menu"> {{ menu }} </a>
-
-    <!-- 3. <태그 v-for="(작명(n),순서(i)) in 배열" :key="작명"> {{ 작명/순서 }} -->
-    <!-- <a v-for="(menu,i) in menus" :key="menu"> {{ menu }},{{ i }} </a> -->
   </div>
 
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
 
   <div v-for="(item, i) in 3" :key="item">
-    <h4 class="red" :style="fontColor">{{ products[i] }}</h4>
+    <h4 class="product" :style="fontColor">{{ products[i] }}</h4>
     <p>{{ prices[i] }} 만원</p>
+    <!-- <button @click="reportCnt++">허위매물신고</button> <span>신고수 : {{ reportCnt }}</span> -->
+    <!-- <button @click="increase">허위매물신고</button> <span>신고수 : {{ reportCnt }}</span> -->
+    <button @click="increase(i)">허위매물신고</button> <span>신고수 : {{ reportCnts[i] }}</span>
   </div>
-  <!-- 다른 방법
-        <div v-for="(item, i) in products" :key="item">
-          <h4 class="red" :style="fontColor">{{ item }}</h4>
-          <p>{{ prices[i] }} 만원</p>
-        </div>
-  -->
-  <!-- <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ prices[1] }} 만원</p>
-  </div>
-  <div>
-    <h4 class="red" :style="fontColor">{{ products[2] }}</h4>
-    <p>{{ prices[2] }} 만원</p>
-  </div> -->
 </template>
 
 <script>
 
-// document.getElementById().innerHTML = ?; js 데이터 바인딩 방법
 
 export default {
   name: 'App',
-  data() {  // 데이터 보관, 자료형은 object
+  data() {
     return {
-      prices : [50, 60, 70],  // 자료이름 : 내용
+      prices : [50, 60, 70],
       fontColor : "color : blue",
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      menus : ['Home', 'Shop', 'About']
+      menus : ['Home', 'Shop', 'About'],
+      // reportCnt : 0, 
+      reportCnts : [0, 0, 0]
+    }
+  },
+  methods : { // 함수
+    // increase() { //
+    //   this.reportCnt++;
+    // }
+    increase : function(idx) {
+      this.reportCnts[idx]++;
     }
   },
   components: {
