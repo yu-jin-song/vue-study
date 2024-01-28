158b946b4f9bc9288b0f3c4a233d13c509656765
yu-jin-song <yujinsong.tech@gmail.com>
2024-01-28 22:48:33 +0900

    feat: Apply v-model for month

--- real-estate-mall/src/components/Modal.vue
+++ real-estate-mall/src/components/Modal.vue
@@ -4,7 +4,11 @@
             <img class="img" :src="onerooms[clickNo].image">
             <h4>{{onerooms[clickNo].title}}</h4>
             <p>{{onerooms[clickNo].content}}</p>
-            <p>{{onerooms[clickNo].price}} 원</p>
+            <!-- <input @input="month = $event.target.value" /> -->
+            <!-- <input v-model="month" /> -->
+            <input v-model.number="month" />
+            <!-- <p>{{month}}개월  {{onerooms[clickNo].price * month}} 원</p> -->
+            <p>{{month}}개월  {{onerooms[clickNo].price + month}} 원</p>
             <button @click="$emit('closeModal')">닫기</button>
         </div>
   </div>
@@ -15,6 +19,11 @@
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Modal',
+    data() {
+        return {
+            month : 1
+        }
+    },
     props : {
         onerooms : Array,
         clickNo : Number,
