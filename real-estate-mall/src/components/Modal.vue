5300d49f9ff7197ad266a4f20b88f46505ba46ed
yu-jin-song <yujinsong.tech@gmail.com>
2024-01-30 17:53:40 +0900

    feat: Apply lifecycle hook(beforeUpdated)

--- real-estate-mall/src/components/Modal.vue
+++ real-estate-mall/src/components/Modal.vue
@@ -21,16 +21,32 @@ export default {
     name: 'Modal',
     data() {
         return {
-            month : 1
+            month : 1,
+            preventLoop: false
+        }
+    },
+    beforeUpdate() {
+        if(parseInt(this.month) === 2) {
+            alert('2는 입력할 수 없습니다.');
+
+            // lifecycle hook 내에서의 데이터 변경은 권장되지 않는 방법(사이드 이펙트 및 무한루프, 성능 저하등의 이슈 발생)
+            // 데이터 변경은 methods, computed, watch 속성을 통해 처리하는 것을 권장
+            this.preventLoop = true; // 무한 루프 방지를 위해 플래그 설정
+            this.month = 1;
         }
     },
     watch : {
         month(data) {
+            if(this.preventLoop) {
+                this.preventLoop = false;
+                return;
+            }
+
             if(data === '') {    // 데이터 입력이 없는 경우
                 return; // 아무런 처리도 하지 않음
             }
 
-            if(data.isNaN() || data.includes(' ')) {  // 데이터가 숫자가 아닌 경우
+            if(Number.isNaN(data) || data.includes(' ')) {  // 데이터가 숫자가 아닌 경우
                 alert('숫자만 입력해주세요.');
 
                 if(/\d/.test(data)) { // 숫자가 존재하는 경우
