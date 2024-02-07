61e04bfe0cb0f21642f5a05615b557811d4f2ed6
yu-jin-song <yujinsong.tech@gmail.com>
2024-02-07 22:09:17 +0900

    feat: Apply tab

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -9,7 +9,7 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :posts="posts" />
+  <Container :posts="posts" :step="step" />
   <button @click="more">더보기</button>
 
   <div class="footer">
@@ -18,6 +18,15 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+<!-- tab 기능 테스트
+  <div v-if="selectedTabNum === 0">내용0</div>
+  <div v-if="selectedTabNum === 1">내용1</div>
+  <div v-if="selectedTabNum === 2">내용2</div>
+  <button @click="selectedTabNum = 0">버튼0</button>
+  <button @click="selectedTabNum = 1">버튼1</button>
+  <button @click="selectedTabNum = 2">버튼2</button>
+ -->
 </template>
 
 <script>
@@ -30,7 +39,9 @@ export default {
   data() {
     return {
       posts: posts,
-      clickMoreCnt: 0
+      clickMoreCnt: 0,
+      // selectedTabNum: 0,
+      step : 2
     }
   },
   components: {
@@ -38,13 +49,9 @@ export default {
   },
   methods: {
     more() {
-      // axios.get('https://codingapple1.github.io/vue/more0.json')
-      // .then(function(result) {
-      //   console.log(result.data);
-      // });
+      // axios.get(`https://codingapple1.github.io/vue/more${this.clickMoreCnt}.json`)
       axios.get('https://codingapple1.github.io/vue/more' + this.clickMoreCnt + '.json')
       .then((result) => {
-        // console.log(result.data);
         this.posts.push(result.data);
         this.clickMoreCnt++;
       })
--- vuestagram/src/components/Container.vue
+++ vuestagram/src/components/Container.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
-    <!-- <Post :post="posts[0]" />
-    <Post :post="posts[1]" />
-    <Post :post="posts[2]" /> -->
-    <Post :post="posts[i]" v-for="(item, i) in posts" :key="i"/>
+    <!-- 게시글 -->
+    <div v-if="step === 0">
+      <Post :post="posts[i]" v-for="(item, i) in posts" :key="i" />
+    </div>
+
+    <!-- 필터 선택 페이지 -->
+    <!-- 방법1.
+    <div class="upload-image" v-if="step === 1"></div>
+    <div class="filters" v-if="step === 1">
+     -->
+    <!-- 방법2. wrapper -->
+    <div v-if="step === 1">
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+
+    <!-- 글 작성 페이지 -->
+    <!-- 방법1.
+    <div class="upload-image" v-if="step === 2"></div>
+    <div class="write" v-if="step === 2">
+     -->
+    <!-- 방법2. wrapper -->
+    <div v-if="step === 2">
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,13 +43,15 @@ import Post from "./Post.vue";
 export default {
   name: "Container",
   props: {
-    posts: Array
+    posts: Array,
+    step: Number,
   },
   components: {
     Post: Post,
-  }
+  },
 };
 </script>
 
 <style>
+@import "../assets/css/sytle.css";
 </style>
